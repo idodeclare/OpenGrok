96d64c9018e4e29af323d7b3a2609e05054fbefd
Chris Fraire <cfraire@me.com>
2018-01-28 09:58:47 -0600

    ... I S S U E # 6 0 7 : Add classes to support Forth analysis

--- opengrok-indexer/src/main/java/org/opengrok/indexer/analysis/AnalyzerGuru.java
+++ opengrok-indexer/src/main/java/org/opengrok/indexer/analysis/AnalyzerGuru.java
@@ -77,6 +77,7 @@ import org.opengrok.indexer.analysis.erlang.ErlangAnalyzerFactory;
 import org.opengrok.indexer.analysis.executables.ELFAnalyzerFactory;
 import org.opengrok.indexer.analysis.executables.JarAnalyzerFactory;
 import org.opengrok.indexer.analysis.executables.JavaClassAnalyzerFactory;
+import org.opengrok.indexer.analysis.forth.ForthAnalyzerFactory;
 import org.opengrok.indexer.analysis.fortran.FortranAnalyzerFactory;
 import org.opengrok.indexer.analysis.golang.GolangAnalyzerFactory;
 import org.opengrok.indexer.analysis.haskell.HaskellAnalyzerFactory;
@@ -309,7 +310,8 @@ public class AnalyzerGuru {
                 new PascalAnalyzerFactory(env),
                 new AdaAnalyzerFactory(env),
                 new RubyAnalyzerFactory(env),
-                new EiffelAnalyzerFactory(env)
+                new EiffelAnalyzerFactory(env),
+                new ForthAnalyzerFactory(env)
             };
 
             for (FileAnalyzerFactory analyzer : analyzers) {
@@ -342,7 +344,7 @@ public class AnalyzerGuru {
      * {@link FileAnalyzerFactory} subclasses are revised to target more or
      * different files.
      * @return a value whose lower 32-bits are a static value
-     * 20171230_00
+     * 20180209_00
      * for the current implementation and whose higher-32 bits are non-zero if
      * {@link #addExtension(java.lang.String, org.opengrok.indexer.analysis.FileAnalyzerFactory)}
      * or
@@ -350,7 +352,7 @@ public class AnalyzerGuru {
      * has been called.
      */
     public long getVersionNo() {
-        final int ver32 = 20171230_00; // Edit comment above too!
+        final int ver32 = 20180209_00; // Edit comment above too!
         long ver = ver32;
         if (customizationHashCode != 0) {
             ver |= (long)customizationHashCode << 32;
---
+++ opengrok-indexer/src/test/java/org/opengrok/indexer/analysis/forth/ForthXrefTest.java
@@ -0,0 +1,136 @@
+/*
+ * CDDL HEADER START
+ *
+ * The contents of this file are subject to the terms of the
+ * Common Development and Distribution License (the "License").
+ * You may not use this file except in compliance with the License.
+ *
+ * See LICENSE.txt included in this distribution for the specific
+ * language governing permissions and limitations under the License.
+ *
+ * When distributing Covered Code, include this CDDL HEADER in each
+ * file and include the License file at LICENSE.txt.
+ * If applicable, add the following below this CDDL HEADER, with the
+ * fields enclosed by brackets "[]" replaced with your own identifying
+ * information: Portions Copyright [yyyy] [name of copyright owner]
+ *
+ * CDDL HEADER END
+ */
+
+/*
+ * Copyright (c) 2012, 2016, Oracle and/or its affiliates. All rights reserved.
+ * Portions Copyright (c) 2018, Chris Fraire <cfraire@me.com>.
+ */
+
+package org.opengrok.indexer.analysis.forth;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.io.StringWriter;
+import java.io.Writer;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import org.junit.BeforeClass;
+import org.opengrok.indexer.analysis.Definitions;
+import org.opengrok.indexer.analysis.FileAnalyzer;
+import org.opengrok.indexer.analysis.WriteXrefArgs;
+import org.opengrok.indexer.analysis.Xrefer;
+import org.opengrok.indexer.configuration.RuntimeEnvironment;
+import static org.opengrok.indexer.util.CustomAssertions.assertLinesEqual;
+import static org.opengrok.indexer.util.StreamUtils.copyStream;
+import static org.opengrok.indexer.util.StreamUtils.readTagsFromResource;
+
+/**
+ * Tests the {@link ForthXref} class.
+ */
+public class ForthXrefTest {
+
+    private static RuntimeEnvironment env;
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+        env = RuntimeEnvironment.getInstance();
+    }
+
+    @Test
+    public void sampleTest() throws IOException {
+        writeAndCompare("analysis/forth/sample.fs",
+                "analysis/forth/sample_xref.html",
+                readTagsFromResource(
+                        "analysis/forth/sampletags"),
+                        287);
+    }
+
+    @Test
+    public void shouldCloseTruncatedStringSpan() throws IOException {
+        writeAndCompare("analysis/forth/truncated.fs",
+                "analysis/forth/truncated_xref.html",
+                null, 1);
+    }
+
+    private void writeAndCompare(String sourceResource, String resultResource,
+            Definitions defs, int expLOC) throws IOException {
+
+        InputStream res = getClass().getClassLoader().getResourceAsStream(
+                sourceResource);
+        assertNotNull(sourceResource + " should get-as-stream", res);
+
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        int actLOC = writeForthXref(new PrintStream(baos), res, defs);
+        res.close();
+
+        InputStream exp = getClass().getClassLoader().getResourceAsStream(
+                resultResource);
+        assertNotNull(resultResource + " should get-as-stream", exp);
+        byte[] expraw = copyStream(exp);
+        exp.close();
+        baos.close();
+
+        String ostr = new String(baos.toByteArray(), "UTF-8");
+        String gotten[] = ostr.split("\n");
+
+        String estr = new String(expraw, "UTF-8");
+        String expected[] = estr.split("\n");
+
+        assertLinesEqual("Forth xref", expected, gotten);
+        assertEquals("Forth LOC", expLOC, actLOC);
+    }
+
+    private int writeForthXref(PrintStream oss, InputStream iss,
+        Definitions defs) throws IOException {
+
+        oss.print(getHtmlBegin());
+
+        Writer sw = new StringWriter();
+        ForthAnalyzerFactory fac = new ForthAnalyzerFactory(env);
+        FileAnalyzer analyzer = fac.getAnalyzer();
+        analyzer.setScopesEnabled(true);
+        analyzer.setFoldingEnabled(true);
+        WriteXrefArgs wargs = new WriteXrefArgs(
+                new InputStreamReader(iss, "UTF-8"), sw);
+        wargs.setDefs(defs);
+        Xrefer xref = analyzer.writeXref(wargs);
+        oss.print(sw.toString());
+
+        oss.print(getHtmlEnd());
+        return xref.getLOC();
+    }
+
+    private static String getHtmlBegin() {
+        return "<!DOCTYPE html>\n" +
+            "<html>\n" +
+            "<head>\n" +
+            "<meta charset=\"UTF-8\">\n" +
+            "<title>sampleFile - OpenGrok cross reference" +
+            " for /sampleFile</title></head><body>\n";
+    }
+
+    private static String getHtmlEnd() {
+        return "</body>\n" +
+            "</html>\n";
+    }
+}
