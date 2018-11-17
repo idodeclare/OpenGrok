7314d1c3c833cf82969d11b5e602f8e16757009c
Chris Fraire <cfraire@me.com>
2018-11-17 09:34:43 -0600

    ... add logging

--- opengrok-indexer/src/main/java/org/opengrok/indexer/configuration/RuntimeEnvironment.java
+++ opengrok-indexer/src/main/java/org/opengrok/indexer/configuration/RuntimeEnvironment.java
@@ -213,6 +213,7 @@ public final class RuntimeEnvironment {
             configLock.readLock().lock();
             return getFieldValue(configuration, fieldName);
         } catch (IOException e) {
+            LOGGER.log(Level.WARNING, e.getMessage());
             return null;
         } finally {
             configLock.readLock().unlock();
@@ -230,6 +231,7 @@ public final class RuntimeEnvironment {
             configLock.readLock().lock();
             return getFieldValue(configuration, fieldName);
         } catch (IOException e) {
+            LOGGER.log(Level.WARNING, e.getMessage());
             throw new IOException("getter", e);
         } finally {
             configLock.readLock().unlock();
